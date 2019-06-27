fcf77cb93b8d3f396e9fd940e7c2e9a46cec1e28
Kaviza90 <vieralaqueviera@gmail.com>
2019-06-27 18:43:47 -0500

    agregando el responsive web

--- src/main.js
+++ src/main.js
@@ -1,27 +1,21 @@
 const championsArray = Object.values(LOL.data);
-document.getElementById("sortByAttack").addEventListener("click", window.dataManager.sort);
+document
+  .getElementById("sortByAttack")
+  .addEventListener("click", window.dataManager.sort);
 const characterBio = () => {
-  document.getElementById("champions").style.display="none";
-  document.getElementById("details").style.display="block";
-}
+  document.getElementById("champions").style.display = "none";
+  document.getElementById("details").style.display = "block";
+};
 
 const showAll = () => {
-<<<<<<< HEAD
-    document.getElementById("sortByAttack").style.display="block";
-    championsArray.forEach(champion => {
-=======
+  document.getElementById("sortByAttack").style.display = "block";
   championsArray.forEach(champion => {
->>>>>>> 7ad48145c5d891ff3badb3ec5b60130a0d4c156a
     let championPic = document.createElement("IMG");
     championPic.setAttribute("src", champion.img);
     championPic.setAttribute("width", "60");
     championPic.setAttribute("height", "60");
     champText.appendChild(championPic);
     champText.innerHTML +=
-<<<<<<< HEAD
-      " " + champion.name + ", " + champion.title + ". " + champion.tags + ".  ";
-    let bioButton = document.createElement("button");
-=======
       " " +
       champion.name +
       ", " +
@@ -29,7 +23,7 @@ const showAll = () => {
       ". " +
       champion.tags +
       ".  ";
->>>>>>> 7ad48145c5d891ff3badb3ec5b60130a0d4c156a
+    let bioButton = document.createElement("button");
     bioButton.innerHTML = "Biography";
     bioButton.className = "Bio";
     champText.appendChild(bioButton);
@@ -40,34 +34,7 @@ const showAll = () => {
     champText.appendChild(statsButton);
     champText.innerHTML += "<br>";
   });
-<<<<<<< HEAD
-
-}
-
-
-document.getElementById("allChampButton").addEventListener("click", showAll);
-document.getElementById("tanksButton").addEventListener("click", window.dataManager.sort);
-document.getElementById("magesButton").addEventListener("click", window.dataManager.sort);
-document.getElementById("assassinsButton").addEventListener("click", window.dataManager.sort);
-document.getElementById("marksmenButton").addEventListener("click", window.dataManager.sort);
-document.getElementById("supportButton").addEventListener("click", window.dataManager.sort);
-document.getElementById("statButton").addEventListener("click", window.dataManager.sort);
-=======
-  const characterBio = () => {
-    alert("si");
-    document.getElementById("champText").style.display = "none";
-    document.getElementById("characters").style.display = "block";
-  };
-  document
-    .getElementsbyClassName("Bio")
-    .addEventListener("click", characterBio);
 };
-<script>
-function mostrar(){
-document.getElementById('nuevaimg').innerHTML = '<img src="ruta-imagen.jpg">';}
-</script>
-<a href="javascript:mostrar()">Mostrar</a>
-
 
 document.getElementById("allChampButton").addEventListener("click", showAll);
 document
@@ -88,4 +55,3 @@ document
 document
   .getElementById("statButton")
   .addEventListener("click", window.dataManager.sort);
->>>>>>> 7ad48145c5d891ff3badb3ec5b60130a0d4c156a
