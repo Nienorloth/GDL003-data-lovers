26441d410c8b0d75eb19858d5e0f48379a18c1c8
Kaviza90 <vieralaqueviera@gmail.com>
2019-07-04 15:28:49 -0500

    cambios menores

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 let championsArray = Object.values(LOL.data);
 const attackSort = document.getElementById("sortByAttack");
 const defenseSort = document.getElementById("sortByDefense");
@@ -16,56 +15,59 @@ const statButton = document.getElementById("statButton");
 const champImage = document.getElementById("champImage");
 
 const statsScreen = () => {
-  document.getElementById("champions").style.display="none";
-  document.getElementById("champText").style.display="none";
-  document.getElementById("statistics").style.display="block";
-}
-attackSort.addEventListener("click", (event) => {
-   let stats = event.target.dataset.stats
-   window.dataManager.sortData(stats);
-});
-defenseSort.addEventListener("click", (event) => {
-   let stats = event.target.dataset.stats
-   window.dataManager.sortData(stats);
-});
-magicSort.addEventListener("click", (event) => {
-   let stats = event.target.dataset.stats;
-   window.dataManager.sortData(stats);
-});
-difficultySort.addEventListener("click", (event) => {
+  document.getElementById("champions").style.display = "none";
+  document.getElementById("champText").style.display = "none";
+  document.getElementById("statistics").style.display = "block";
+};
+
+champText.innerHTML = championsArray.attack;
+
+attackSort.addEventListener("click", event => {
   let stats = event.target.dataset.stats;
   window.dataManager.sortData(stats);
 });
-aZ.addEventListener("click", (event) => {
+defenseSort.addEventListener("click", event => {
+  let stats = event.target.dataset.stats;
+  window.dataManager.sortData(stats);
+});
+magicSort.addEventListener("click", event => {
+  let stats = event.target.dataset.stats;
+  window.dataManager.sortData(stats);
+});
+difficultySort.addEventListener("click", event => {
+  let stats = event.target.dataset.stats;
+  window.dataManager.sortData(stats);
+});
+aZ.addEventListener("click", event => {
   let stats = event.target.dataset.stats;
   window.dataManager.sortData(stats);
 });
 
-allChamp.addEventListener("click",(event) => {
+allChamp.addEventListener("click", event => {
   let stats = event.target.dataset.stats;
   window.dataManager.filterData(stats);
 });
-tanksFilter.addEventListener("click", (event) => {
+tanksFilter.addEventListener("click", event => {
   let stats = event.target.dataset.stats;
   window.dataManager.filterData(stats);
 });
-magesFilter.addEventListener("click", (event) => {
+magesFilter.addEventListener("click", event => {
   let stats = event.target.dataset.stats;
   window.dataManager.filterData(stats);
 });
-assassinsFilter.addEventListener("click", (event) => {
+assassinsFilter.addEventListener("click", event => {
   let stats = event.target.dataset.stats;
   window.dataManager.filterData(stats);
 });
-marksmenFilter.addEventListener("click", (event) => {
+marksmenFilter.addEventListener("click", event => {
   let stats = event.target.dataset.stats;
   window.dataManager.filterData(stats);
 });
-fightersFilter.addEventListener("click", (event) => {
+fightersFilter.addEventListener("click", event => {
   let stats = event.target.dataset.stats;
   window.dataManager.filterData(stats);
 });
-supportFilter.addEventListener("click", (event) => {
+supportFilter.addEventListener("click", event => {
   let stats = event.target.dataset.stats;
   window.dataManager.filterData(stats);
 });
