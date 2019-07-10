f9dd64e3ce595abc5e8f762ed3d6358ea4efce51
Kaviza90 <vieralaqueviera@gmail.com>
2019-07-10 14:06:40 -0500

    se agregan los test

--- test/data.spec.js
+++ test/data.spec.js
@@ -2,12 +2,33 @@ require("../src/data.js");
 const LOL = require("..src/data/lol/lol.json");
 
 describe('filterData()', () => {
-  it('debería devolver true para function filterdata'); () =>{
-});
-  it('debería devolver false para sortData');
+    it('debería devolver true para function filterData', () => {
+    assertEqual(function('filterData'), true);
+  });
+
+  it('debería devolver false para function filterData', () => {
+    assertEqual(function('filterData'), false);
+  });
 });
 
+describe('sortData()', () => {
+  it('debería devolver true para function sortData', () => {
+  assertEqual(function('sortData'), true);
+});
 
+it('debería devolver false para function sortData', () => {
+  assertEqual(function('sortData'), false);
+  });
+});
+
+describe('computeStats()', () => {
+  it('debería devolver true para function compuStast', () => {
+  assertEqual(function('compuStast'), true);
+});
+it('debería devolver false para function compuStast', () => {
+  assertEqual(function('compuStast'), false);
+  });
+});
 /*describe("filterData", () => {
   it("is a function", () => {
     expect(typeof dataManager.filterData).toBe("function");
